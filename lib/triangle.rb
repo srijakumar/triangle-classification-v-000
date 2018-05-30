cf928eabd38a52549ccba68d3e550ccaec7f1dff
Srija S <srijakumar7@gmail.com>
2018-05-30 21:49:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,28 @@ class Triangle
     @arg3 = arg3
   end
 
+  def kind
+    validate_triangle
 
+    if arg1 == arg2 && arg2 == arg3
+      :equilateral
+    elsif arg1 == arg2 || arg2 == arg3 || arg1 == arg3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+      real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+      [a, b, c].each { |s| real_triangle << false if s <= 0 }
+      raise TriangleError if real_triangle.include?(false)
+    end
+
+
+
+  class TriangleError < StandardError
+      # triangle error code
+    end
 
 end
