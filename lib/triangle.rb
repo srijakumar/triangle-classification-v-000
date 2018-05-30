c5f90b4730b238d4d0ffcd8501edc4973f60bb2c
Srija S <srijakumar7@gmail.com>
2018-05-30 21:40:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
   # write code here
+  attr_accessor :arg1, :arg2, :arg3
+
+  def initialize (arg1, arg2, arg3)
+    @arg1 = arg1
+    @arg2 = arg2
+    @arg3 = arg3
+  end
+
+
+  
 end
